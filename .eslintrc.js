7610845aa387d6090972e00de239081cd55efd95
LESSSON <674373781@qq.com>
2020-06-22 20:18:56 +0800

    code register

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
-module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint',
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+// module.exports = {
+//   root: true,
+//   parserOptions: {
+//     parser: 'babel-eslint',
+//     sourceType: 'module'
+//   },
+//   env: {
+//     browser: true,
+//     node: true,
+//     es6: true,
+//   },
+//   extends: ['plugin:vue/recommended', 'eslint:recommended'],
 
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
@@ -195,4 +195,4 @@ module.exports = {
   //   }],
   //   'array-bracket-spacing': [2, 'never']
   // }
-}
+// }
--- src/views/project/register.vue
+++ src/views/project/register.vue
@@ -1,13 +1,76 @@
 <template>
-  <div>
-    <el-input v-model="value" />
-    <el-button
-      type="primary"
-      @click="GetPositionsByLevelType(value)"
-    >
-      提交
-    </el-button>
-  </div>
+  <el-container  style="margin-left:10px">
+    <el-header>
+      <h1>注册</h1>
+    </el-header>
+    <el-main>
+      <el-form :inline="false" :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="请选择您所在的主体:" prop="staffId" >
+            <el-select v-model="ruleForm.staffIds" placeholder="请选择" multiple @visible-change="getRemoteAddr($event,remote_addr)" style="width:300px" filterable>
+                <el-option
+                v-for="item in remote_addrs"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="请选择您所在的主体分支:" prop="staffId" >
+            <el-select v-model="ruleForm.staffIds" placeholder="请选择" multiple @visible-change="getRemoteAddr($event,remote_addr)" style="width:300px" filterable>
+                <el-option
+                v-for="item in remote_addrs"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="姓名" prop="name" >
+            <el-input v-model="ruleForm.name" ></el-input>
+          </el-form-item>
+          <el-form-item label="用户名" prop="userName" >
+            <el-input v-model="ruleForm.userName" ></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password" >
+            <el-input v-model="ruleForm.password" ></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="passwordToo" >
+            <el-input v-model="ruleForm.passwordToo" ></el-input>
+          </el-form-item>
+          <el-form-item label="请选择您的学历:" prop="academic" >
+            <el-select v-model="ruleForm.academic" placeholder="请选择" multiple @visible-change="getRemoteAddr($event,remote_addr)" style="width:300px" filterable>
+                <el-option
+                v-for="item in academics"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="请选择您的民族:" prop="nation" >
+            <el-select v-model="ruleForm.nation" placeholder="请选择" multiple @visible-change="getRemoteAddr($event,remote_addr)" style="width:300px" filterable>
+                <el-option
+                v-for="item in nations"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+                </el-option>
+            </el-select>
+          </el-form-item>
+      </el-form>
+
+
+    <div>
+      <el-button
+        type="primary"
+        @click="GetPositionsByLevelType(value)"
+      >
+        提交
+      </el-button>
+    </div>
+    </el-main>
+  </el-container>
+
 </template>
 
 
@@ -18,7 +81,12 @@ export default {
   name: "Register",
   data() {
     return {
-      value: 0
+      ruleForm: {
+      staffId:1,
+      name:"",
+      },
+      value: 0,
+
     };
   },
   methods: {
@@ -26,7 +94,7 @@ export default {
       const info = { levelType: value };
       getPositionsByLevelType(info).then(response => {
         this.value = response.data;
-        console.log(response.data);
+        // console.log(response.data);
       });
     }
   }
