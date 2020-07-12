bb137ed8c633ea5c9be810ae638c0813c82b539e
LESSSON <674373781@qq.com>
2020-07-12 15:16:05 +0800

    add disPro

---
+++ src/api/disPro.js
@@ -0,0 +1,114 @@
+import request from '@/utils/request'
+
+// 新增历史病害立项信息
+export function addDisProInfo(
+  auditName,
+  auditUserName,
+  contractSeg,
+  disRegId,
+  proDate,
+  proDes,
+  proId,
+  proName,
+  proStatus,
+  regDate,
+  reqConDate,
+  shtNameSup,
+  userName,
+) {
+  return request({
+    url: '/management/company/add-dis-pro-info',
+    method: 'post',
+    data: {
+      auditName,
+      auditUserName,
+      contractSeg,
+      disRegId,
+      proDate,
+      proDes,
+      proId,
+      proName,
+      proStatus,
+      regDate,
+      reqConDate,
+      shtNameSup,
+      userName,
+    },
+    baseURL: 'http://47.102.101.25:8088'
+  })
+}
+
+
+// 删除历史病害立项信息
+export function deleteDisProInfo(iproIdd) {
+  return request({
+    url: '/management/company/delete-dis-pro-info',
+    method: 'delete',
+    params: {
+      proId
+    },
+    baseURL: 'http://47.102.101.25:8088'
+  })
+}
+
+
+// 根根据立项名称获取立项信息
+export function getDisProInfoByProName(proName) {
+  return request({
+    url: '/management/company/get-dis-pro-info-by-proName',
+    method: 'get',
+    params: {
+      proName
+    },
+    baseURL: 'http://47.102.101.25:8088'
+  })
+}
+// 按登记时间分页获取立项信息
+export function getDisProInfos(limit, offset) {
+  return request({
+    url: '/management/company/get-dis-pro-infos',
+    method: 'get',
+    params: {
+      proName
+    },
+    baseURL: 'http://47.102.101.25:8088'
+  })
+}
+
+// 修改历史病害立项信息
+export function modifyDisProInfo(
+  auditName,
+  auditUserName,
+  contractSeg,
+  disRegId,
+  proDate,
+  proDes,
+  proId,
+  proName,
+  proStatus,
+  regDate,
+  reqConDate,
+  shtNameSup,
+  userName,
+) {
+  return request({
+    url: '/management/company/modify-dis-file-info',
+    method: 'put',
+    params: {
+      auditName,
+      auditUserName,
+      contractSeg,
+      disRegId,
+      proDate,
+      proDes,
+      proId,
+      proName,
+      proStatus,
+      regDate,
+      reqConDate,
+      shtNameSup,
+      userName,
+    },
+    baseURL: 'http://47.102.101.25:8088'
+  })
+}
